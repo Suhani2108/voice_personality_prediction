a0c354b88b288125186da5a274e8453496a15e93
Suhani2108 <suhanibhardwaj087@gmail.com>
2026-04-29 10:28:50 -0700

    Update make_model.py

--- make_model.py
+++ make_model.py
@@ -2,13 +2,12 @@ import joblib
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 
-# FIXED: correct feature size (same as app)
-X = np.random.rand(300, 22)
-y = np.random.choice(["happy", "sad", "angry", "neutral"], 300)
+X = np.random.rand(200, 22)
+y = np.random.choice(["happy", "sad", "angry", "neutral"], 200)
 
-model = RandomForestClassifier(n_estimators=200)
+model = RandomForestClassifier(n_estimators=100)
 model.fit(X, y)
 
 joblib.dump(model, "emotion_model.pkl")
 
-print("✅ Fresh model created successfully")
+print("✅ Model created")
